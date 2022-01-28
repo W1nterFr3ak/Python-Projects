4ec2401d10906c0955ad1538f5b195b71321e04a
CHRIS BYRON (Int0x80) <WinterFreak@protonmail.com>
2022-01-28 09:59:14 +0000

    server v1.1
    threading

--- PyChan/server.v1.0.py
+++ PyChan/server.v1.0.py
@@ -1,40 +1,51 @@
+from threading import Thread
 import socket
 import sys
 
-#check if sufficient parameters are met.
-if len(sys.argv) <2:
-    print("Usage: server.py ip port ")
-    exit()
+#client handler
+def Handler(client):
+    while True:
+        msg = client.recv(1024)
+        if msg != b'quit':
+            print(msg)
+        else:
+            client.close()
+            break
 
-#banner.
-print("\n\t########  ###  ###  ########  ###  ###  ########  ###    ###")
-print("\t########  ###  ###  ########  ###  ###  ###  ###  ## #   ###")
-print("\t########  ###  ###  ##        ########  ###  ###  ### #  ###    server v1.0")
-print("\t########  ########  ##        ########  ########  ###  # ###    by P4rsz and W1nterFr3ak")
-print("\t####           ###  ########  ###  ###  ###  ###  ###   # ##    email:parsz@protonmail.com,WinterFreak@protonmail.com")
-print("\t####      ########  ########  ###  ###  ###  ###  ###    ###\n")
 
-#port and host
-host = str(sys.argv[1])
-port = int(sys.argv[2])
-
-#server object, binding.
-server = socket.socket(socket.AF_INET)
-server.bind((host, port))
 
 #accept connections
-while True:
-    try:
-        #listen for connections
-        print(f"[-] Listening on {host} : {port}")
-        server.listen(10)
-        #accept incoming connections, send a welcome message, notify us of the connection then close connection.
+def accept_conn():
+    while True:
         conn, addr = server.accept()
         conn.send(b' Welcome to the server')
         print(f"[-] {addr} connected.")
-        conn.close()
+        Thread(target=Handler, args=(conn, )).start()
+
+
+if __name__ == "__main__":
+    #banner.
+    print("\n\t########  ###  ###  ########  ###  ###  ########  ###    ###")
+    print("\t########  ###  ###  ########  ###  ###  ###  ###  ## #   ###")
+    print("\t########  ###  ###  ##        ########  ###  ###  ### #  ###    server v1.0")
+    print("\t########  ########  ##        ########  ########  ###  # ###    by P4rsz and W1nterFr3ak")
+    print("\t####           ###  ########  ###  ###  ###  ###  ###   # ##    email:parsz@protonmail.com,WinterFreak@protonmail.com")
+    print("\t####      ########  ########  ###  ###  ###  ###  ###    ###\n")
+
+    #check if sufficient parameters are met.
+    if len(sys.argv) <2:
+        print("Usage: server.py ip port ")
         exit()
-    #handle interrupts.
-    except KeyboardInterrupt:
-        print("\n\tExiting")
-        exit()
+
+    #port and host
+    host = str(sys.argv[1])
+    port = int(sys.argv[2])
+
+    #server object, binding.
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+
+    server.listen(10)
+    print(f"[-] Listening on {host} : {port}")
+    connect = Thread(target=accept_conn)
+    connect.start()
